ae5620f0d24211224ac98d84f924df9760992850
Bekarys <bekaryserezep05@gmail.com>
2025-01-03 15:07:27 +0500

    Add new sound

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import logo from './assets/logo.png';
-import sound from './sounds/timer.mp3'
+import sound from './sounds/endgame.mp3';
 import FullScreen from './Fullscreen';
 import './App.css';
 
@@ -8,31 +8,30 @@ function Timer() {
   const [time, setTime] = useState(150); // 2:30 = 150 seconds
   const [isRunning, setIsRunning] = useState(false);
   const [isAlert, setIsAlert] = useState(false);
-  
+
   // Create a reference for the timer sound
   const timerSound = useRef(new Audio(sound));
 
   // Timer logic
   useEffect(() => {
     let timer;
-    
-    if (isRunning && time > 0) {
-      // Play sound once when timer starts and make sure it's not interrupted
-      if (timerSound.current.paused) {
-        timerSound.current.play();
-      }
-      timerSound.current.loop = true;  // Loop the sound while timer is running
 
+    if (isRunning && time > 0) {
       timer = setInterval(() => {
         setTime((prevTime) => prevTime - 1);
+
         if (time === 30) {
-          setIsAlert(true); // Trigger the alert animation when there are 30 seconds left
+          setIsAlert(true); // Trigger the alert animation when 30 seconds are left
         }
       }, 1000);
     } else if (time === 0) {
+      // Play the sound once when the timer ends
+      timerSound.current.play();
       setIsRunning(false);
-      setIsAlert(false); // Stop animation after timer finishes
-      timerSound.current.loop = false; // Stop the loop when timer reaches 0
+      setIsAlert(false);
+
+      // Automatically reset the timer after the sound ends
+      timerSound.current.onended = handleReset;
     }
 
     return () => clearInterval(timer);
@@ -52,7 +51,7 @@ function Timer() {
     setTime(150);
     setIsRunning(false);
     setIsAlert(false);
-    timerSound.current.pause(); // Stop sound when reset
+    timerSound.current.pause(); // Stop sound if it's still playing
     timerSound.current.currentTime = 0; // Reset sound position to the start
   };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Timer from './Timer';
 import reportWebVitals from './reportWebVitals';
 
